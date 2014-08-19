9fbc18ed77fea724bdc2455bd9803f9915458835
RocMax <jichibang@gmail.com>
2014-08-19 23:33:48 +0800

    ItemData complete

--- Classes/ItemData.cpp
+++ Classes/ItemData.cpp
@@ -7,3 +7,54 @@
 //
 
 #include "ItemData.h"
+#include "DBOperations.h"
+
+static ItemData* instance=NULL;
+
+ItemData* ItemData::getItemData(int ItemID){
+    if (!instance) {
+        instance=new ItemData();
+    }
+    
+    //读入对应ItemID的数据
+    char* errorMsg;
+    //读取数据库
+    sqlite3* pDB=DBOperation::openDB();
+    CCString* sqlcs=CCString::createWithFormat("select * from Item where ItemID=%d",ItemID);
+    const char* sql=sqlcs->getCString();
+    CCLog(sql);
+    int result=sqlite3_exec(pDB, sql, sqlite_callback, NULL, &errorMsg);
+    if (result!=SQLITE_OK) {
+        CCLog(errorMsg);
+    }
+    
+    sqlite3_close(pDB);
+    
+    return instance;
+
+    
+}
+
+int ItemData::sqlite_callback(void *pv, int column, char **dbResult, char **col){
+    instance->setItemName(dbResult[1]);
+    instance->setItemPic(dbResult[2]);
+    instance->setItemPrice(atoi(dbResult[3]));
+    instance->setEquipType(atoi(dbResult[4]));
+    instance->setisunique((bool)atoi(dbResult[5]));
+    instance->setItemHP(atof(dbResult[6]));
+    instance->setItemATK(atof(dbResult[7]));
+    instance->setItemDEF(atof(dbResult[8]));
+    instance->setItemAGI(atof(dbResult[9]));
+    instance->setItemLUK(atof(dbResult[10]));
+    instance->setHPAbsorb(atof(dbResult[11]));
+    instance->setExpRate(atof(dbResult[12]));
+    instance->setCriRate(atof(dbResult[13]));
+    instance->setCriDam(atof(dbResult[14]));
+    instance->setComboRate(atof(dbResult[15]));
+    instance->setEncounterRate(atof(dbResult[16]));
+    instance->setSpeedRate(atof(dbResult[17]));
+    instance->setItemDropRate(atof(dbResult[18]));
+    instance->setGoldDropRate(atof(dbResult[19]));
+    
+    return 0;
+}
--- Classes/ItemData.h
+++ Classes/ItemData.h
@@ -15,27 +15,38 @@
 USING_NS_CC;
 
 class ItemData:public CCObject{
-    CC_SYNTHESIZE_READONLY(int, ItemID, ItemID);
-    CC_SYNTHESIZE_READONLY(char*, ItemName, ItemName);
-    CC_SYNTHESIZE_READONLY(char*, ItemPic, ItemPic)
-    CC_SYNTHESIZE_READONLY(int, ItemPrice, ItemPrice);
-    CC_SYNTHESIZE_READONLY(int, EquipType, EquipType);
-    CC_SYNTHESIZE_READONLY(bool, isUnique, isunique);
-    CC_SYNTHESIZE_READONLY(float, ItemHP, ItemHP);
-    CC_SYNTHESIZE_READONLY(float, ItemATK, ItemATK);
-    CC_SYNTHESIZE_READONLY(float, ItemDEF, ItemDEF);
-    CC_SYNTHESIZE_READONLY(float, ItemAGI, ItemAGI);
-    CC_SYNTHESIZE_READONLY(float, ItemLUK, ItemLUK);
-    CC_SYNTHESIZE_READONLY(float, HPAbsorb, HPAbsorb);
-    CC_SYNTHESIZE_READONLY(float, ExpRate, ExpRate);
-    CC_SYNTHESIZE_READONLY(float, CriRate, CriRate);
-    CC_SYNTHESIZE_READONLY(float, CriDam, CriDam);
-    
-    
-    
+    CC_SYNTHESIZE(int, ItemID, ItemID);
+    CC_SYNTHESIZE(char*, ItemName, ItemName);
+    CC_SYNTHESIZE(char*, ItemPic, ItemPic)
+    CC_SYNTHESIZE(int, ItemPrice, ItemPrice);
+    CC_SYNTHESIZE(int, EquipType, EquipType);
+    CC_SYNTHESIZE(bool, isUnique, isunique);
+    CC_SYNTHESIZE(float, ItemHP, ItemHP);
+    CC_SYNTHESIZE(float, ItemATK, ItemATK);
+    CC_SYNTHESIZE(float, ItemDEF, ItemDEF);
+    CC_SYNTHESIZE(float, ItemAGI, ItemAGI);
+    CC_SYNTHESIZE(float, ItemLUK, ItemLUK);
+    CC_SYNTHESIZE(float, HPAbsorb, HPAbsorb);
+    CC_SYNTHESIZE(float, ExpRate, ExpRate);
+    CC_SYNTHESIZE(float, CriRate, CriRate);
+    CC_SYNTHESIZE(float, CriDam, CriDam);
+    CC_SYNTHESIZE(float, ComboRate, ComboRate);
+    CC_SYNTHESIZE(float, EncounterRate, EncounterRate);
+    CC_SYNTHESIZE(float, SpeedRate, SpeedRate);
+    CC_SYNTHESIZE(float, ItemDropRate, ItemDropRate);
+    CC_SYNTHESIZE(float, GoldGropRate, GoldDropRate);
     
 public:
     static ItemData* getItemData(int ItemID);
+private:
+    static int sqlite_callback(
+                               void* pv,    /* 由 sqlite3_exec() 的第四个参数传递而来 */
+                               int column,        /* 表的列数 */
+                               char** dbResult,    /* 指向查询结果的指针数组, 可以由 sqlite3_column_text() 得到 */
+                               char** col        /* 指向表头名的指针数组, 可以由 sqlite3_column_name() 得到 */
+    );
+
+    
     
 };
 
